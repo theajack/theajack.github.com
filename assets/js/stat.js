7d536c271e7f9cc2df95bd7a25f9a6c8d6b26da0
theajack <1506282385@qq.com>
2017-06-09 10:46:17 +0800

    17-6-9

--- assets/js/stat.js
+++ assets/js/stat.js
@@ -1,18 +1,28 @@
+//name属性    state_name     #statName
 J.ready(function(){
   var name;
-  if(typeof stat_name=="undefined"){
-    var s_n=J.id("statName");
-    if(s_n==null){
-      name=J.tag("title").txt();
-    }else{
-      name=s_n.txt();
+  var ss=J.tag("script");
+  for(var i=0;i<ss.length;i++){
+    if(ss[i].hasAttr("name")){
+      name=ss[i].attr("name");
+      break;
     }
-  }else{
-    name=stat_name;
+  }
+  if(typeof name=="undefined"){
+    if(typeof stat_name=="undefined"){
+      var s_n=J.id("statName");
+      if(s_n==null){
+        name=J.tag("title").txt();
+      }else{
+        name=s_n.txt();
+      }
+    }else{
+      name=stat_name;
+    }
   }
   J.jsonp({
     url:"http://15h97945z7.iok.la/stat.aspx",
-    data:{name:name},
+    data:{type:"add",name:name},
     success:function(data){},
     time:20000,
     timeout:function(err){},
